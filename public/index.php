659b835f0427cce0dd8dbced3b42ca6ff4e1d69d
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2024-12-04 22:21:04 +0200

    Episode3 Updates

--- public/index.php
+++ public/index.php
@@ -1,10 +1,15 @@
 <?php
 
+
+define("ROOT", dirname(__DIR__));
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 
 $request = \RLC\Framework\Http\Request::getGlobals();
 
-dd($request);
 
-echo "Hello world";
+$content = include ROOT."/src/views/home.php";
+$response = new \RLC\Framework\Http\Response($content, 200);
+
+
+$response->send();
---
+++ src/views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Username and Email Form</title>
+</head>
+<body>
+<h1>Register</h1>
+<form action="/submit" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" placeholder="Enter your username" required>
+    <br><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" placeholder="Enter your email" required>
+    <br><br>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
