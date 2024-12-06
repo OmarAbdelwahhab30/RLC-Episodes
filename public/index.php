149333a0703e831b9210b01f445c71fe5561b39a
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2024-12-06 16:11:44 +0200

    Episode4

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 $request = \RLC\Framework\Http\Request::getGlobals();
 
 
-$content = include ROOT."/src/views/home.php";
-$response = new \RLC\Framework\Http\Response($content, 200);
 
+$kernel = new \RLC\Framework\Http\Kernel();
+$response = $kernel->handle($request);
 
 $response->send();
