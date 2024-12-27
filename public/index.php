ff2d91925a95409727531dba2d3958850ed15404
omarkishk <omarabdelwahhabkishk2000@gmail.com>
2024-12-27 20:38:59 +0200

    Episode12

--- public/index.php
+++ public/index.php
@@ -4,7 +4,10 @@
 define("ROOT", dirname(__DIR__));
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
+
 $container = require ROOT."/config/services.php";
+dd(($container->get(\Doctrine\DBAL\Connection::class))
+->executeQuery("select * from users")->fetchAssociative());
 
 $request = \RLC\Framework\Http\Request::getGlobals();
 
